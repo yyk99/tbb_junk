c3e8bd02716e498be8e440aa095ba537770ce97d
Yuri Y. Kuznetsov <yyk@kestrel>
2022-10-02 12:07:29 -0400

    Compiled fig_1_05.cpp

--- pro-TBB/ch01/fig_1_05.cpp
+++ pro-TBB/ch01/fig_1_05.cpp
@@ -22,14 +22,14 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 SPDX-License-Identifier: MIT
 */
 
-#include <pstl/algorithm>
-#include <pstl/execution>
+#include <algorithm>
+#include <execution>
 #include <iostream>
 #include <vector>
 
 int main() { 
   std::vector<std::string> v = { " Hello ", " Parallel STL! " };
-  std::for_each(pstl::execution::par, v.begin(), v.end(),
+  std::for_each(std::execution::par, v.begin(), v.end(),
     [](std::string& s) { std::cout << s << std::endl; }
   ); 
   return 0;
